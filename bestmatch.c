837249a9576d02759775c3e938ca89eb8e25cea8
Jari Aalto <jari.aalto@cante.net>
2010-03-04 10:00:31 +0200

    {bestmatch.c,extract.c,merge.c} restore original 0.6
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- bestmatch.c
+++ bestmatch.c
@@ -346,10 +346,7 @@ void remap(struct best *best, int cnt,
 	int pa,pb;
 	pa=pb=0;
 
-	if (a1.elcnt == 0 && a2.elcnt == 0) return;
-
-	for (b=1; b<cnt; b++)
-	   if (best[b].val>0) {
+	for (b=1; b<cnt; b++) {
 #if 0
 		printf("best %d,%d  %d,%d\n",
 		       best[b].xlo,best[b].ylo,
